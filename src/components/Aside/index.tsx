0d1e9b685f24f972aec51303de7adb793235d8c6
Tales Palma <tales.palma_2022@outlook.com>
2024-06-19 16:50:09 -0300

    New inco code link

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
 import styles from './aside.module.css'
+import logo from './icon.svg'
 export const Aside = () => {
   return (
     <aside className={styles.Aside_desktop}>
-      <img src="assets/icon.svg" alt="Not found icon" className={styles.Logo} />
+      <Image src={logo} alt='Logo site' className={styles.Logo} />
     </aside>
   )
 }
