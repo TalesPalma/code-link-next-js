d8885cbe79ae27e2ff017e7a9f5682ee40da79f6
Tales Palma <tales.palma_2022@outlook.com>
2024-06-25 14:14:54 -0300

    Add navigates with links and pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { CardPost } from "@/components/CardCode";
 import style from './page.module.css'
+import logger from "@/logger";
+import Link from "next/link";
 
-
-
-export interface Author {
-  id: string,
-  name: string,
-  username: string,
-  avatar: string,
+export interface PostPages {
+  "first": number,
+  "prev": number | null,
+  "next": number,
+  "last": number,
+  "pages": number,
+  "items": number,
+  data: Post[]
 }
-
 
 export interface Post {
   id: string,
@@ -21,29 +23,40 @@ export interface Post {
   author: Author
 }
 
+export interface Author {
+  id: string,
+  name: string,
+  username: string,
+  avatar: string,
+}
 
 
 
 
-
-async function get_all_post() {
-  const response = await fetch('http://localhost:3042/posts', { next: { revalidate: 3600 } });
+async function get_all_post(page: string) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`, { next: { revalidate: 3600 } });
 
   if (!response.ok) {
-    console.log("Ops ocorreu algum erro na get_all_post() !!!")
+    logger.error("Ops ocorreu algum erro na get_all_post() !!!")
   }
+  logger.info("Post obtidos com sucesso");
   return response.json();
 }
 
 
-export default async function Home() {
-  const posts: Post[] = await get_all_post()
+export default async function Home({ searchParams }: { searchParams: { [key: string]: string } }) {
+  const currentPage = searchParams?.page || "1"
+  const posts: PostPages = await get_all_post(currentPage)
   console.log(posts)
   return (
     <main className={style.page_container}>
 
       <div className={style.card_container}>
-        {posts.map((item: Post) => <CardPost key={item.id} post={item} />)}
+        {posts.data.map((item: Post) => <CardPost key={item.id} post={item} />)}
+      </div>
+      <div className={style.navigates_container}>
+        {posts.prev && <Link href={`/?page=${posts.prev}`} className={style.navigates_link}>Pagina anterior</Link>}
+        {posts.next && <Link href={`/?page=${posts.next}`} className={style.navigates_link}>Proxima pagina</Link>}
       </div>
 
     </main >
